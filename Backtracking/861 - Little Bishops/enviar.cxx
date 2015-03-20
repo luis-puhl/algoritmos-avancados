4a93c5374e21877c783b5a7d906fe3a3355c1b18
Luís Puhl <luispuhl@gmail.com>
2015-03-20 10:05:25 -0300

    Little Bishops enviado, utilziado método com todas as respostas

--- Backtracking/861 - Little Bishops/enviar.cxx
+++ Backtracking/861 - Little Bishops/enviar.cxx
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <cmath>
 using namespace std;
+typedef unsigned int tpos;
+typedef int ttab;
 
-int isDiagonal(int src, int dest, int boardSize){
-	int ax, ay, bx, by;
+int isDiagonal(ttab src, ttab dest, ttab boardSize){
+	ttab ax, ay, bx, by;
 	
 	if (src == dest){
 		return 1;
 	}
 	
-	ax = src / boardSize; // linha
-	ay = src % boardSize; // coluna
+	ax = src / boardSize;
+	ay = src % boardSize;
 	
-	bx = dest / boardSize; // linha
-	by = dest % boardSize; // coluna
+	bx = dest / boardSize;
+	by = dest % boardSize;
 	
 	if (ax-ay == bx-by || ax+ay == bx+by){
 		return 1;
@@ -22,24 +26,24 @@ int isDiagonal(int src, int dest, int boardSize){
 	return 0;
 }
 
-long int sol = 0;
-void process_solution(vector<int> a){
+tpos sol = 0;
+void process_solution(vector<ttab> a){
 	sol++;
 }
 
-vector<int> *filter(vector<int> *d, int bisp, int boardsize){
-	vector<int> *nd;
-	nd = new vector<int>();
+vector<ttab> *filter(vector<ttab> *d, ttab bisp, ttab boardsize){
+	vector<ttab> *nd;
+	nd = new vector<ttab>();
 	
 	if (d == NULL){
-		int numOpcoes = boardsize * boardsize;
-		d = new vector<int>();
+		ttab numOpcoes = boardsize * boardsize;
+		d = new vector<ttab>();
 		for (int i = 0; i < numOpcoes; i++){
 			d->push_back(i);
 		}
 	}
 	
-	for (vector<int>::iterator it = d->begin(); it < d->end(); ++it){
+	for (vector<ttab>::iterator it = d->begin(); it < d->end(); ++it){
 		if (*it < 0 || bisp >= *it){
 			continue;
 		}
@@ -55,51 +59,91 @@ vector<int> *filter(vector<int> *d, int bisp, int boardsize){
 	return nd;
 }
 
-void backtrack(vector<int> a, vector<int> *d, int k_bis, int n_tab, int k){
-	vector<int> *nd;
+unsigned long int nodes = 0;
+void backtrack(vector<ttab> a, vector<ttab> *d, ttab k_bis, ttab n_tab, ttab k){
+	vector<ttab> *nd;
+	nodes++;
+	
 	if ( ((int)a.size()) == k_bis ){
 		process_solution(a);
-	} else {
-		//~ d = filter(d, k, n_tab);
+	} else if ( ((int)a.size()) == k_bis - 1 ){
 		nd = filter(d, k, n_tab);
-		if ( d->size() < (k_bis - a.size()) ){
-			return;
-		}
-		for (vector<int>::iterator i = nd->begin(); i < nd->end(); i++){
-			if ( *i > (n_tab * n_tab) - (k_bis - (int)a.size()) ){
-				return;
-			}
+		for (vector<ttab>::iterator i = nd->begin(); i < nd->end(); i++){
 			a.push_back(*i);
-			backtrack(a, nd, k_bis, n_tab, *i);
+			process_solution(a);
 			a.pop_back();
+		}
+		delete nd;
+	} else {
+		nd = filter(d, k, n_tab);
+		
+		if (
+			nd->size()
+			>= (k_bis - a.size())
+		){
+			for (vector<ttab>::iterator i = nd->begin(); i < nd->end(); i++){
+				if ( *i > (n_tab * n_tab) - (k_bis - (int)a.size()) ){
+					break;
+				}
+				a.push_back(*i);
+				backtrack(a, nd, k_bis, n_tab, *i);
+				a.pop_back();
+			}
 		}
 		delete nd;
 	}
 }
 
-long long int bishopsDyna[9][9];
-long long int bishops(int k_bis, int n_tab){
-	vector<int> a, *d;
+unsigned int bishopsDyna[65][9] = 
+ {1, 1, 1, 1, 1, 1, 1, 1, 1,
+0, 1, 4, 9, 16, 25, 36, 49, 64,
+0, 0, 4, 26, 92, 240, 520, 994, 1736,
+0, 0, 0, 26, 232, 1124, 3896, 10894, 26192,
+0, 0, 0, 8, 260, 2728, 16428, 70792, 242856,
+0, 0, 0, 0, 112, 3368, 39680, 282248, 1444928,
+0, 0, 0, 0, 16, 1960, 53744, 692320, 5599888,
+0, 0, 0, 0, 0, 440, 38368, 1022320, 14082528,
+0, 0, 0, 0, 0, 32, 12944, 867328, 22522960,
+0, 0, 0, 0, 0, 0, 1600, 389312, 22057472,
+0, 0, 0, 0, 0, 0, 64, 81184, 12448832,
+0, 0, 0, 0, 0, 0, 0, 5792, 3672448,
+0, 0, 0, 0, 0, 0, 0, 128, 489536,
+0, 0, 0, 0, 0, 0, 0, 0, 20224,
+0, 0, 0, 0, 0, 0, 0, 0, 256};
+
+tpos bishops(int k_bis, int n_tab){
+	vector<ttab> a, *d;
 	sol = 0;
+	nodes = 0;
 	d = NULL;
-	if (k_bis <= n_tab && n_tab <= 8){
+	if (k_bis <= n_tab*n_tab){
 		if (bishopsDyna[k_bis][n_tab] > 0){
 			sol = bishopsDyna[k_bis][n_tab];
+		} else if(n_tab > 1 && k_bis > (n_tab-1)*2){
+			sol = 0;
+		} else if (k_bis == 0){
+			sol = 1;
+		} else if (k_bis == 1){
+			sol = n_tab * n_tab;
+		} else if (n_tab == 1){
+			sol = 1;
 		} else {
 			backtrack(a, d, k_bis, n_tab, -1);
 		}
 	}
-	printf("F(k_bis, n_tab) (%d, %d) = %ld\n", k_bis, n_tab, sol);
+	bishopsDyna[k_bis][n_tab] = sol;
 	return sol;
 }
 
 int main(int argc, char **argv){
+	
 	int k, n;
-	while (scanf("%d %d", &k, &n)){
+	while(cin >> n >> k){
 		if (n==0 && k==0){
 			break;
 		}
-		printf("%lld\n", bishops(k, n) );
+		printf("%u\n", bishops(k, n) );
 	}
+	
 	return 0;
 }
--- Backtracking/861 - Little Bishops/nova.cxx
+++ Backtracking/861 - Little Bishops/nova.cxx
@@ -2,12 +2,18 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <cmath>
 using namespace std;
 #define MAXCANDIDATES 64
 #define ECHO printf("#\n");
+typedef unsigned int tpos;
+typedef int ttab;
 
-int isDiagonal(int src, int dest, int boardSize){
-	int ax, ay, bx, by;
+//~ #define DEBUG
+//~ #define SHOW_RESULT
+
+int isDiagonal(ttab src, ttab dest, ttab boardSize){
+	ttab ax, ay, bx, by;
 	
 	if (src == dest){
 		return 1;
@@ -25,11 +31,11 @@ int isDiagonal(int src, int dest, int boardSize){
 	return 0;
 }
 
-long long int sol = 0;
-void process_solution(vector<int> a){
+tpos sol = 0;
+void process_solution(vector<ttab> a){
 	sol++;
 	#ifdef DEBUG
-	printf("result #%ld", sol);
+	printf("result #%u", sol);
 	printf("\tBISPOS { ");
 	for (vector<int>::iterator i = a.begin(); i < a.end(); ++i){
 		printf("%d, ", *i);
@@ -38,14 +44,14 @@ void process_solution(vector<int> a){
 	#endif
 }
 
-vector<int> *filter(vector<int> *d, int bisp, int boardsize){
-	vector<int> *nd;
-	nd = new vector<int>();
+vector<ttab> *filter(vector<ttab> *d, ttab bisp, ttab boardsize){
+	vector<ttab> *nd;
+	nd = new vector<ttab>();
 	
 	#ifdef DEBUG
 	printf("\t\tprevCand { ");
 	if (d != NULL){
-		for (vector<int>::iterator i = d->begin(); i < d->end(); i++){
+		for (vector<ttab>::iterator i = d->begin(); i < d->end(); i++){
 			printf("%d, ", *i);
 		}
 	}
@@ -53,14 +59,14 @@ vector<int> *filter(vector<int> *d, int bisp, int boardsize){
 	#endif
 	
 	if (d == NULL){
-		int numOpcoes = boardsize * boardsize;
-		d = new vector<int>();
+		ttab numOpcoes = boardsize * boardsize;
+		d = new vector<ttab>();
 		for (int i = 0; i < numOpcoes; i++){
 			d->push_back(i);
 		}
 	}
 	
-	for (vector<int>::iterator it = d->begin(); it < d->end(); ++it){
+	for (vector<ttab>::iterator it = d->begin(); it < d->end(); ++it){
 		// se o valor for negativo ou o bispo alocado anteriormente é maior que a casa analisada
 		if (*it < 0 || bisp >= *it){
 			continue;
@@ -76,7 +82,7 @@ vector<int> *filter(vector<int> *d, int bisp, int boardsize){
 	
 	#ifdef DEBUG
 	printf("\t\tCand { ");
-	for (vector<int>::iterator i = nd->begin(); i < nd->end(); i++){
+	for (vector<ttab>::iterator i = nd->begin(); i < nd->end(); i++){
 		printf("%d, ", *i);
 	}
 	printf("}\n");
@@ -85,10 +91,9 @@ vector<int> *filter(vector<int> *d, int bisp, int boardsize){
 	return nd;
 }
 
-
-unsigned long long int nodes = 0;
-void backtrack(vector<int> a, vector<int> *d, int k_bis, int n_tab, int k){
-	vector<int> *nd;
+unsigned long int nodes = 0;
+void backtrack(vector<ttab> a, vector<ttab> *d, ttab k_bis, ttab n_tab, ttab k){
+	vector<ttab> *nd;
 	nodes++;
 	#ifdef DEBUG
 	printf("\n\tlen %lu, poss %lu\n", a.size(), d!=NULL?d->size():0);
@@ -102,50 +107,78 @@ void backtrack(vector<int> a, vector<int> *d, int k_bis, int n_tab, int k){
 	
 	if ( ((int)a.size()) == k_bis ){
 		process_solution(a);
+	} else if ( ((int)a.size()) == k_bis - 1 ){
+		nd = filter(d, k, n_tab);
+		for (vector<ttab>::iterator i = nd->begin(); i < nd->end(); i++){
+			a.push_back(*i);
+			process_solution(a);
+			a.pop_back();
+		}
+		delete nd;
 	} else {
 		nd = filter(d, k, n_tab);
+		
 		// se as possiveis alocacoes sao menores que as casas para uma solucao
-		if ( nd->size() < (k_bis - a.size()) ){
-			return;
-		}
-		for (vector<int>::iterator i = nd->begin(); i < nd->end(); i++){
-			#ifdef DEBUG
-			printf("\tk= %lud, BISPO= %d\n", a.size(), *i);
-			#endif
-			// se a casa atual for mais alta do que as casas necessarias para achar uma solucao
-			if ( *i > (n_tab * n_tab) - (k_bis - (int)a.size()) ){
-				return;
+		if (
+			nd->size() // possibilidades de alocar
+			>= (k_bis - a.size()) // quantos bispos falta
+		){
+			for (vector<ttab>::iterator i = nd->begin(); i < nd->end(); i++){
+				#ifdef DEBUG
+				printf("\tk= %lud, BISPO= %d\n", a.size(), *i);
+				#endif
+				// se a casa atual for mais alta do que as casas necessarias para achar uma solucao
+				if ( *i > (n_tab * n_tab) - (k_bis - (int)a.size()) ){
+					break;
+				}
+				a.push_back(*i);
+				backtrack(a, nd, k_bis, n_tab, *i);
+				a.pop_back();
 			}
-			a.push_back(*i);
-			backtrack(a, nd, k_bis, n_tab, *i);
-			a.pop_back();
 		}
 		delete nd;
 	}
 }
 
-long long int bishopsDyna[65][9];
-long long int bishops(int k_bis, int n_tab){
-	vector<int> a, *d;
+tpos bishopsDyna[15][9];
+tpos bishops(int k_bis, int n_tab){
+	if (k_bis >= 15){
+		return 0;
+	}
+	
+	vector<ttab> a, *d;
 	sol = 0;
 	nodes = 0;
 	d = NULL;
+	
 	if (k_bis <= n_tab*n_tab){
 		if (bishopsDyna[k_bis][n_tab] > 0){
+			// prog dinamica sempre ajuda =D
 			sol = bishopsDyna[k_bis][n_tab];
-		} else /* devia ter uma terceira opçao aqui, mas esqueci qual era */{
+		} else if(n_tab > 1 && k_bis > (n_tab-1)*2){
+			// limite de alocação, a solução é zero para este caso 
+			sol = 0;
+		} else if (k_bis == 0){
+			sol = 1;
+		} else if (k_bis == 1){
+			sol = n_tab * n_tab;
+		} else if (n_tab == 1){
+			sol = 1;
+		//~ } else if (n_tab != 1 && k_bis == (2 * n_tab) -1 ){
+			//~ sol = ((unsigned long long int) 2) << n_tab ;
+		} else {
 			backtrack(a, d, k_bis, n_tab, -1);
 		}
 	}
 	#ifdef SHOW_RESULT
-	printf("F(k_bis, n_tab) (%d, %d) = %lld with %llu nodes\n", k_bis, n_tab, sol, nodes);
+	printf("F(k_bis, n_tab) (%d, %d) = %u \nwith %lu nodes\n", k_bis, n_tab, sol, nodes);
 	#endif
 	bishopsDyna[k_bis][n_tab] = sol;
 	return sol;
 }
 
 void init(){
-	for (int i = 0; i <= 64; i++){
+	for (int i = 0; i <= 14; i++){
 		for (int j = 0; j <= 8; j++){
 			bishopsDyna[i][j] = 0;
 		}
@@ -155,7 +188,7 @@ void init(){
 int main(int argc, char **argv){
 	init();
 	
-	bishops(8, 8);
+	//~ bishops(8, 8);
 	
 	//~ int k, n;
 	//~ while(cin >> n >> k){
@@ -166,32 +199,25 @@ int main(int argc, char **argv){
 		//~ 
 	//~ }
 	
-	//~ printf("long long int bishopsDyna[8][8] = {");
-	//~ for (int i = 0; i <= 8; i++){
-		//~ for (int j = 0; j <= 8; j++){
-			//~ long long int k = bishops(i, j);
-			//~ printf("%lld", k);
-			//~ if ( i!= 8 && j != 8){
-				//~ printf(", ");
-			//~ }
-		//~ }
-	//~ }
-	//~ printf("};\n");
-	//~ 
-	//~ for (int i = 0; i <= 8; i++){
-		//~ for (int j = 0; j <= 8; j++){
-			//~ long long int k = bishopsDyna[i][j];
-			//~ printf("%lld\n", k);
-		//~ }
-	//~ }
+	for (int i = 0; i <= 14; i++){
+		for (int j = 0; j <= 8; j++){
+			bishops(i, j);
+		}
+	}
+	printf("\n");
 	
-	//~ bishops(2,2);
-	//~ bishops(3,3);
-	//~ bishops(3,4);
-	//~ bishops(4,4);
-	//~ bishops(5,5);
-	//~ bishops(6,6);
-	//~ bishops(7,7);
-	//~ bishops(9,8);
+	printf("unsigned int bishopsDyna[15][9] = {");
+	for (int i = 0; i <= 14; i++){
+		for (int j = 0; j <= 8; j++){
+			tpos k = bishopsDyna[i][j];
+			printf("%u", k);
+			if ( i != 14 && j != 8){
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	printf("};\n");
+	
 	return 0;
 }
